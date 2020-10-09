aec39653995d590c6642827aa620ce304582492a
Sarah Purvis <onplanetoff@darwin.attlocal.net>
2020-10-09 14:41:17 -0700

    adding tests for smellController: smellFeed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -19,16 +19,16 @@ const userController = {
     // TODO: render an error message when login fails
     userLogIn: (username, password, done) => {
         User.findOne({ username: username, password: password }, (err, user) => {
-            console.log(`User ${username} attempted to log in`);
+            // console.log(`User ${username} attempted to log in`);
             if (err) {
                 return done(err);
             }
             if (!user) {
-                console.log('username does not exist');
+                // console.log('username does not exist');
                 return done(null, false);
             }
             if (user.password !== password) {
-                console.log('incorrect password');
+                // console.log('incorrect password');
                 return done(null, false)
             }
             return done(null, user);
--- tests/controllers/smellController.spec.js
+++ tests/controllers/smellController.spec.js
@@ -1,5 +1,6 @@
 const smellController = require('../../controllers/smellControllers');
 const Smell = require('../../models/Smell');
+const mongoose = require('mongoose');
 
 describe('Controller: smellController', () => {
     describe('Function: createSmell', () => {
@@ -46,6 +47,62 @@ describe('Controller: smellController', () => {
             smellController.createSmell(fakeRequest, fakeResponse);
 
             expect(fakeResponse.redirect).toHaveBeenCalledWith(expectedRedirect);
+        });
+    });
+    describe('Function: smellFeed', () => {
+        let fakeRequest;
+        let fakeResponse;
+        beforeEach(() => {
+            fakeRequest = {
+                user: {
+                    username: 'username'
+                }
+            }
+            fakeResponse = {}
+        });
+        it('sends an error if the Smell model throws an error', () => {
+            const expectedError = 'error';
+            fakeResponse.send = jest.fn();
+            jest.spyOn(mongoose.Query.prototype, 'exec').mockImplementation((callback) => {
+                callback(expectedError);
+            });
+
+            smellController.smellFeed(fakeRequest, fakeResponse);
+
+            expect(fakeResponse.send).toHaveBeenCalledWith(expectedError);
+        });
+        it('renders the smell feed with the appropriate data', () => {
+            const docs = [{
+                _id: 5,
+                uplick: [ {username: 'uplick user1'}, {username: 'uplick user2'} ],
+                downpoop: [ {username: 'downpoop user1'}, {username: 'downpoop user2'} ]
+            }];
+            const expectedSmellsArr = [{
+                _id: 5,
+                uplick: [ {username: 'uplick user1'}, {username: 'uplick user2'} ],
+                downpoop: [ {username: 'downpoop user1'}, {username: 'downpoop user2'} ],
+                uplickUrl: '/uplick/5',
+                downpoopUrl: '/downpoop/5',
+                kickUrl: '/kick/5',
+                deleteUrl: '/deleteSmell/5',
+                uplickers: 'uplick user1, uplick user2',
+                downpoopers: 'downpoop user1, downpoop user2'
+            }];
+            jest.spyOn(process, 'cwd').mockReturnValue('');
+            const expectedResObject = {
+                title: 'Feed',
+                username: 'username',
+                smellsArr: expectedSmellsArr
+            }
+            fakeResponse.render = jest.fn();
+            jest.spyOn(mongoose.Query.prototype, 'exec').mockImplementation((callback) => {
+                callback(null, docs);
+            });
+
+
+            smellController.smellFeed(fakeRequest, fakeResponse);
+
+            expect(fakeResponse.render).toHaveBeenCalledWith('/views/feed', expectedResObject);
         });
     });
 });

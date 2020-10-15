8d1ac2b87c623192bbe75288dbc5a116a5ca9b35
Sarah Purvis <onplanetoff@darwin.attlocal.net>
2020-10-14 17:24:22 -0700

    moving routes from index.js to routes.js

---
+++ routes.js
@@ -0,0 +1,43 @@
+const userController = require('./controllers/userControllers');
+const smellController = require('./controllers/smellControllers');
+const ensureAuthenticated = require('./middlewares/ensureAuthenticated');
+
+const configureRoutes = (app, authenticationMiddleware) => {
+    app.get('/', (_, res) => {
+        res.render(process.cwd() + '/views/login', {title: 'Login'});
+    });
+    
+    app.post('/login', authenticationMiddleware, (_, res) => {
+        res.redirect('/feed');
+    });
+    
+    app.route('/register')
+        .get((_, res) => {
+            res.render(process.cwd() + '/views/register', {title: 'Register'});
+        });
+    
+    app.get('/checkUsername/:username', userController.checkUserName);
+
+    app.post('/welcome', userController.welcomeUser);
+    
+    app.get('/feed', ensureAuthenticated, smellController.smellFeed);
+       
+    app.post('/createSmell', ensureAuthenticated, smellController.createSmell);
+    
+    app.post('/kick/:smellID', ensureAuthenticated, smellController.kickSmell);
+    
+    app.get('/uplick/:smellID', ensureAuthenticated, smellController.uplick);
+    
+    app.get('/downpoop/:smellID', ensureAuthenticated, smellController.downpoop);
+    
+    app.get('/deleteSmell/:smellID', ensureAuthenticated, smellController.deleteSmell);
+    
+    app.get('/profile', ensureAuthenticated, smellController.profileFeed);
+
+    app.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+}
+
+module.exports = configureRoutes;
